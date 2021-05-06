517e9bff56b1973b83f8948150a63e0f9efc1657
PaoloGerosa <64699038+PaoloGerosa@users.noreply.github.com>
2021-05-06 09:54:13 +0200

    Update Credenziali.py

--- Credenziali.py
+++ Credenziali.py
@@ -6,7 +6,7 @@ def inserisciCredenzialiDatabase():
     mydb = connector.connect(
         host='localhost',     # if not changed: localhost
         user='root',          # if not changed: root
-        passwd='Chitarra1998',    # password mysql
+        passwd='',    # password mysql
         database='casadb')
 
     return mydb
